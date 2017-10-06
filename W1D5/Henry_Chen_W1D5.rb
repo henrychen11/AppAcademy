65b8b1778f46b74f7a14e2cc1d6a53b0234b29ab
Henry Chen <henrychen1@gmail.com>
2017-10-05 18:51:47 -0700

    hw update

--- W1D5/Henry_Chen_W1D5.rb
+++ W1D5/Henry_Chen_W1D5.rb
@@ -7,6 +7,7 @@ class Stack
   def add(el)
     # adds an element to the stack
     @ivar << el
+    el
   end
 
   def remove
@@ -27,6 +28,7 @@ class Queue
 
   def enqueue(el)
     @ivar << el
+    el
   end
 
   def dequeue
@@ -42,15 +44,46 @@ end
 class Map
 
   def initialize
+    @ivar = []
   end
 
   def assign(key, value)
+    pairs = @ivar.index {|pair| pair[0] == key}
+    value_holder = @ivar[pairs][1]
+    if pairs.count > 1
+      value_holder = value
+    else
+      @ivar << [key, value]
+    end
+    [key, value]
+
   end
 
   def lookup(key)
+    @ivar.each do {|pair| return pair[1] if pair[0] == key }
+    nil
   end
 
   def remove(key)
+    @ivar.reject! {|pair| pair[0] == key}
+    nil
   end
 
+  def show
+    deep_dup(@ivar)
+  end
+
+  def deep_dup(array)
+    result = []
+    array.map do |elem|
+      if elem.is_a(Array)
+        deep_dup(elem)
+      else
+        result << elem
+      end
+    end
+    result
+    end
+
+  end
 end
